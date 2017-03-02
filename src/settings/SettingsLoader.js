f21843cdbb44da6145420489598553822044f8db
Mark Polak <mark@thedutchies.com>
2017-03-02 13:58:47 +0100

    Correct variable name

--- src/settings/SettingsLoader.js
+++ src/settings/SettingsLoader.js
@@ -9,9 +9,9 @@ class SettingsLoader {
     getSystemSetting(settingsKey) {
         return this.api.get([this.BASE_URL, 'api', 'systemSettings', settingsKey].join('/'), { cache: true })
             .then(response => response.data)
-            .then(data => data === '' ? Promise.reject('No value found for ' + key) : Promise.resolve(data))
+            .then(data => data === '' ? Promise.reject('No value found for ' + settingsKey) : Promise.resolve(data))
             .catch(error => {
-                console && console.error && console.error(['Key', key, 'not found'].join(' '));
+                console && console.error && console.error(['Key', settingsKey, 'not found'].join(' '));
                 return Promise.reject(error);
             });
     }
