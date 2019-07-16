a40049511c0fac0c3416b677a56b07e76f24958f
Thomas Zemp <tzemp@baosystems.com>
2019-07-16 14:55:56 +0200

    fix for #30

--- new/src/services/getUrl.service.js
+++ new/src/services/getUrl.service.js
@@ -7,7 +7,7 @@ else serverConfig = devServerConfig;
 
 export function generateUrlMechanism(mechanismCode) {
 	const endpoint =
-		"categoryOptions.json?paging=false&fields=:owner,displayName,name,categories[:owner],categoryOptionCombos[:owner],categoryOptionGroups[:owner],organisationUnits[:owner]";
+		"/categoryOptions.json?paging=false&fields=:owner,displayName,name,categories[:owner],categoryOptionCombos[:owner],categoryOptionGroups[:owner],organisationUnits[:owner]";
 	const filter = "filter=code:eq:" + mechanismCode;
 	return serverConfig.baseUrl + endpoint + "&" + filter;
 }
